750a3bdfbc828597cf0acdde8caf66ef11644827
4Azgin <doctorwhofr@protonmail.com>
2020-08-07 18:55:54 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => 'forge',
             'username' => 'root',
-            'password' => 'DVnm4NFJmtHWdmg1olOF',
+            'password' => 'RH1Fa6jUQ8BlcUi2Hizx',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
