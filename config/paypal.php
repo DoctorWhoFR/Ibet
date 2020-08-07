8874d5246dc467364be6b22101d0158d8ed5b1b7
4Azgin <doctorwhofr@protonmail.com>
2020-08-07 20:21:19 +0200

    Update paypal.php

--- config/paypal.php
+++ config/paypal.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'client_id' => env('PAYPAL_CLIENT_ID',''),
-    'secret' => env('PAYPAL_SECRET',''),
+    'client_id' => "AR0PVFsjenb6cw1MZ1y8TXMAoHwq1J3XVpGhT792xWXSxsPyOu8Auy0EXD9_x6ixrJ8zBrOj5Z0LYlEd",
+    'secret' => "ED_r5GW8w5AqFN9TGKCy4OHzJy1-sUQLIZdeaZcJBxQln_fC0uAnLq4HujuwMhXZ9DirLEVyq7DgjSTx",
     'settings' => array(
-        'mode' => env('PAYPAL_MODE','sandbox'),
+        'mode' => "sandbox",
         'http.ConnectionTimeOut' => 30,
         'log.LogEnabled' => true,
         'log.FileName' => storage_path() . '/logs/paypal.log',
